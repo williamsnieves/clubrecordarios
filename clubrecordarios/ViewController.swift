d079ee5949961387c9d667b0f9c40dbc4ce11cc1
willians <williamsnieves@gmail.com>
2016-11-24 15:50:03 -0400

    Initial Commit

---
+++ clubrecordarios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clubrecordarios
+//
+//  Created by willians on 24/11/16.
+//  Copyright © 2016 clubrecordar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
