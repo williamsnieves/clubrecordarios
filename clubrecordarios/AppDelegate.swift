858e02d5b2388bf66b095675636f559f2df739e9
willians <williamsnieves@gmail.com>
2017-02-21 19:02:17 -0400

    notifications config

--- clubrecordarios/AppDelegate.swift
+++ clubrecordarios/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //FIRApp.configure()
+        FIRApp.configure()
         return true
     }
 
@@ -40,6 +43,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func connectToFBMessaging(){
+        FIRMessaging.messaging().connect{(error) in
+            if(error != nil){
+                print("Unable to connect \(error)")
+            }else{
+                print("connect to FCM")
+            }
+            
+        }
     }
 
 
